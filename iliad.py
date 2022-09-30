976587796fd01f9885eab39dcb81cda8e75e2a3b
todd <todd@penguin>
2022-09-30 13:48:00 -1000

    initial commit

---
+++ iliad.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import random
+
+#Variables
+LINES = 12212 # Number of lines in the text file
+FILE = 'edited' # Name of text file to read from
+lineAmount = 10 # Number of lines to be printed
+printline = random.randint(0, LINES - lineAmount) # Random line to begin printing at
+
+lineCounter = 0
+with open(FILE,'r') as f:
+	with open('daily', 'w') as t:
+		t.write('<link href="txtstyle.css" rel="stylesheet" type="text/css" />\n')
+		for line in f:
+			lineCounter += 1
+			if lineCounter == printline:
+				t.write(line)
+				print(line, end='')
+			elif lineCounter > printline and lineCounter < printline + lineAmount:
+				t.write(line)
+				print(line, end='')
