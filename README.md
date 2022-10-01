2b7ce7666a68221b2b47878ff272cd3813a686c9
todd <todd@penguin>
2022-09-30 17:49:35 -1000

    write to folder

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # bibliomancy
 
 Takes a book text file, chooses random line and displays number of lines chosen by user. Also writes the lines to a simple html file for use on a website.
+Starting with Iliad, more to be added later.
 
 Work in progress.
--- iliad.py
+++ iliad.py
@@ -3,13 +3,13 @@ import random
 
 #Variables
 LINES = 12212 # Number of lines in the text file
-FILE = 'edited' # Name of text file to read from
+FILE = 'edited_texts/iliad.e' # Name of text file to read from
 lineAmount = 10 # Number of lines to be printed
 printline = random.randint(0, LINES - lineAmount) # Random line to begin printing at
 
 lineCounter = 0
 with open(FILE,'r') as f:
-	with open('daily', 'w') as t:
+	with open('html/iliad.html', 'w') as t:
 		t.write('<link href="txtstyle.css" rel="stylesheet" type="text/css" />\n')
 		for line in f:
 			lineCounter += 1
