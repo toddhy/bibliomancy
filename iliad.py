da716c446be30c3a51677dddd1e28954fd412681
Todd <toddyosh@gmail.com>
2022-10-02 16:50:20 -1000

    dynamic line count; loop changed to function; new texts added

--- iliad.py
+++ iliad.py
@@ -2,22 +2,29 @@
 import random
 
 #Variables
-LINES = 12212 # Number of lines in the text file
-FILE = 'edited_texts/iliad.e' # Name of text file to read from
-lineAmount = 10 # Number of lines to be printed
-printline = random.randint(0, LINES - lineAmount) # Random line to begin printing at
+FILE = 'aeneid'	# Name of text file to read from
+read_dir = 'edited_texts/'
+write_dir = 'html/'
+lineAmount = 10		# Number of lines to be printed
 
-lineCounter = 0
-with open(FILE,'r') as f:
-	with open('html/iliad.html', 'w') as t:
-		t.write('<link href="txtstyle.css" rel="stylesheet" type="text/css" />\n')
-		for line in f:
-			lineCounter += 1
-			if lineCounter == printline:
-				t.write(line)
-				t.write('<br>')
-				print(line, end='')
-			elif lineCounter > printline and lineCounter < printline + lineAmount:
-				t.write(line)
-				t.write('<br>')
-				print(line, end='')
+#print(LINES)
+
+def bib(in_File, out_File):
+	LINES = sum(1 for line in open(in_File))	# Count number of lines
+	printline = random.randint(0, LINES - lineAmount) 	# Random line to begin printing at
+
+	lineCounter = 0
+	with open(in_File,'r') as f:
+		with open(out_File, 'w') as t:
+			t.write('<link href="txtstyle.css" rel="stylesheet" type="text/css" />\n')	#HTML at top
+			for line in f:
+				lineCounter += 1
+				if lineCounter >= printline and lineCounter < printline + lineAmount:
+					t.write(line)
+					t.write('<br>')
+					print(line, end='')
+bib(read_dir+FILE+'.e', write_dir+FILE+'.html')
+#if lineCounter == printline:
+#	t.write(line)
+#	t.write('<br>')
+#	print(line, end='')
